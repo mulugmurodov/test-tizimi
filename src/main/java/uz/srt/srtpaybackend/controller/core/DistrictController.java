d58a94fab95b843b1fe8ccbd5147f81799017f3f
m.ulugmurodov <m.ulugmurodov@safenetpay.com>
2022-01-07 10:37:45 +0500

    district created

---
+++ src/main/java/uz/srt/srtpaybackend/controller/core/DistrictController.java
@@ -0,0 +1,41 @@
+package uz.srt.srtpaybackend.controller.core;
+
+import lombok.RequiredArgsConstructor;
+import org.springframework.data.domain.Pageable;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
+import uz.srt.srtpaybackend.enteties.core.District;
+import uz.srt.srtpaybackend.service.core.DistrictService;
+
+
+@RestController
+@RequestMapping("api")
+@RequiredArgsConstructor
+public class DistrictController {
+    public final DistrictService districtService;
+
+    @GetMapping("/district")
+    public ResponseEntity<?> getPage(Pageable pageable){
+        return ResponseEntity.ok(districtService.getPage(pageable));
+    }
+
+    @GetMapping("/district/{id}")
+    public ResponseEntity<?> getOne(@PathVariable Long id){
+        return ResponseEntity.ok(districtService.getOne(id));
+    }
+
+    @PostMapping("/district")
+    public ResponseEntity<?> create(@RequestBody District district){
+        return ResponseEntity.ok(districtService.create(district));
+    }
+
+    @PutMapping("/district/{id}")
+    public ResponseEntity<?> update(@PathVariable Long id, @RequestBody District district){
+        return ResponseEntity.ok(districtService.update(id, district));
+    }
+
+    @DeleteMapping("/district/{id}")
+    public ResponseEntity<?> delete(@PathVariable Long id){
+        return ResponseEntity.ok(districtService.delete(id));
+    }
+}
---
+++ src/main/java/uz/srt/srtpaybackend/service/core/DistrictService.java
@@ -0,0 +1,35 @@
+package uz.srt.srtpaybackend.service.core;
+
+import lombok.RequiredArgsConstructor;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
+import org.springframework.stereotype.Service;
+import uz.srt.srtpaybackend.enteties.core.District;
+import uz.srt.srtpaybackend.repositories.core.DistrictRepository;
+
+@Service
+@RequiredArgsConstructor
+public class DistrictService {
+    private final DistrictRepository districtRepository;
+
+    public Page<District> getPage(Pageable pageable){
+        return districtRepository.findAll(pageable);
+    }
+
+    public District getOne(Long id){
+        return districtRepository.findById(id).orElseThrow(() -> new RuntimeException("Data not found"));
+    }
+
+    public District create(District district){
+        return districtRepository.save(district);
+    }
+
+    public District update(Long id, District district){
+        return districtRepository.save(district);
+    }
+
+    public Boolean delete(Long id) {
+        districtRepository.deleteById(id);
+        return true;
+    }
+}
