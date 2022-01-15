9666821f240b096e5fb11912b4d71f3547b53240
m.ulugmurodov <m.ulugmurodov@safenetpay.com>
2022-01-15 11:46:07 +0500

    User crud created

--- src/main/java/uz/srt/srtpaybackend/controller/core/UserController.java
+++ src/main/java/uz/srt/srtpaybackend/controller/core/UserController.java
@@ -3,16 +3,20 @@ package uz.srt.srtpaybackend.controller.core;
 import lombok.RequiredArgsConstructor;
 import org.springframework.data.domain.Pageable;
 import org.springframework.http.ResponseEntity;
+import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.web.bind.annotation.*;
 import uz.srt.srtpaybackend.dto.UserDTO;
 import uz.srt.srtpaybackend.enteties.core.Test;
 import uz.srt.srtpaybackend.service.core.UserService;
+
+import java.util.Objects;
 
 @RestController
 @RequestMapping("api")
 @RequiredArgsConstructor
 public class UserController {
     private final UserService userService;
+    private final PasswordEncoder passwordEncoder;
 
 
     @GetMapping("/user")
@@ -27,6 +31,9 @@ public class UserController {
 
     @PostMapping("/user")
     public ResponseEntity<?> create(@RequestBody UserDTO userDTO){
+        if (Objects.nonNull(userDTO.getPassword())){
+            userDTO.setEncodedPassword(passwordEncoder.encode(userDTO.getPassword()));
+        }
         return ResponseEntity.ok(userService.create(userDTO));
     }
 
